8317a4d942b3aac5cb2d6fd1c05a043fd27bf613
YOmprakash <omprakashyamavaram@gmail.com>
2024-09-01 22:52:05 +0530

    nothing

--- Models/index.js
+++ Models/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
   role: { type: String, required: true, enum: ['student', 'teacher'] },
   name: { type: String, required: true },
   grade: { type: String }, 
